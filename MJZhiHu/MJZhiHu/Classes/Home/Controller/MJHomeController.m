f0e3fe1453328ca52aefabf3e8d7ad8e8824451b
zhangwb <0dWMZbeL9wCo>
2016-05-26 18:00:18 +0800

    首页编码开始

--- MJZhiHu/MJZhiHu/Classes/Home/Controller/MJHomeController.m
+++ MJZhiHu/MJZhiHu/Classes/Home/Controller/MJHomeController.m
@@ -9,15 +9,18 @@
 #import "MJHomeController.h"
 #import "MJDrawerController.h"
 #import "MJDBManager.h"
+#import <Masonry.h>
+#import <FrameAccessor.h>
 
-#define kNavBarHeight          (56)
-#define kLeftButtonWidth       (kNavBarHeight - 20)
+#define kLeftButtonWidth       (kTopBarHeight - kStatusBarH)
 #define kLeftButtonHeight      (kLeftButtonWidth)
+#define kPicturesViewH         (220)
 
 @interface MJHomeController ()
 
-@property (nonatomic, weak) UITableView *tableView;
 @property (nonatomic, weak) UIView *headerView;
+@property (nonatomic, weak) UIView *picturesView;
+@property (nonatomic, weak) UITableView *tableView;
 @property (nonatomic, weak) UIButton *leftButton;
 
 @end
@@ -28,15 +31,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor whiteColor];
+    self.automaticallyAdjustsScrollViewInsets = NO;
     [self setupNavigationBar];
-    [self headerView];
-    [self leftButton];
     [self setupTableView];
-}
-
-- (void)dealloc {
-    NSLog(@"MJHomeController 被销毁了");
+    [self setupPicturesView];
+    [self setupHeaderView];
+    [self setupLeftButton];
 }
 
 #pragma mark - private methods
@@ -50,7 +50,50 @@
 }
 
 - (void)setupTableView {
-    
+    UITableView *tableView = [[UITableView alloc] init];
+    [self.view addSubview:tableView];
+    [tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.view.mas_top);
+        make.leading.equalTo(self.view.mas_leading);
+        make.trailing.equalTo(self.view.mas_trailing);
+        make.bottom.equalTo(self.view.mas_bottom);
+    }];
+    UIView *tableHeaderView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, kScreenW, kPicturesViewH)];
+    tableHeaderView.backgroundColor = [UIColor brownColor];
+    tableView.tableHeaderView = tableHeaderView;
+    [tableView addObserver:self forKeyPath:@"contentOffset" options:NSKeyValueObservingOptionNew context:nil];
+    _tableView = tableView;
+}
+
+- (void)setupPicturesView {
+    UIView *picturesView = [[UIView alloc] init];
+    picturesView.backgroundColor = [UIColor redColor];
+    [self.view addSubview:picturesView];
+    [picturesView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.view.mas_top);
+        make.leading.equalTo(self.view.mas_leading);
+        make.trailing.equalTo(self.view.mas_trailing);
+        make.height.equalTo(@kPicturesViewH);
+    }];
+    _picturesView = picturesView;
+}
+
+- (void)setupHeaderView {
+    UIView *headerView = [[UIView alloc] init];
+    headerView.frame = CGRectMake(0, 0, kScreenW, kTopBarHeight);
+    headerView.backgroundColor = kNavBarThemeColor;
+    headerView.alpha = 0;
+    [self.view addSubview:headerView];
+    _headerView = headerView;
+}
+
+- (void)setupLeftButton {
+    UIButton *leftButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [leftButton setImage:[UIImage imageNamed:@"Home_Icon"] forState:UIControlStateNormal];
+    leftButton.frame = CGRectMake(10, 20, kLeftButtonWidth, kLeftButtonHeight);
+    [leftButton addTarget:self action:@selector(didClickLeftButton:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:leftButton];
+    _leftButton = leftButton;
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
@@ -60,29 +103,16 @@
     }
 }
 
-#pragma mark - getter & setter
-
-- (UIView *)headerView {
-    if (!_headerView) {
-        UIView *headerView = [[UIView alloc] init];
-        headerView.frame = CGRectMake(0, 0, kScreenW, kNavBarHeight);
-        headerView.backgroundColor = kNavBarThemeColor;
-        [self.view addSubview:headerView];
-        _headerView = headerView;
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context {
+    if (object == self.tableView && [keyPath isEqualToString:@"contentOffset"]) {
+        NSLog(@"object:%@",NSStringFromCGPoint(self.tableView.contentOffset) );
+        CGFloat yOffset = self.tableView.contentOffset.y;
+        [self.picturesView mas_updateConstraints:^(MASConstraintMaker *make) {
+            make.height.equalTo(@(kPicturesViewH - yOffset));
+        }];
+        CGFloat alpha = 0;
+//        alpha = (kPicturesViewH - kTopBarHeight) - yOffset
     }
-    return _headerView;
-}
-
-- (UIButton *)leftButton {
-    if (!_leftButton) {
-        UIButton *leftButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        [leftButton setImage:[UIImage imageNamed:@"Home_Icon"] forState:UIControlStateNormal];
-        leftButton.frame = CGRectMake(10, 20, kLeftButtonWidth, kLeftButtonHeight);
-        [leftButton addTarget:self action:@selector(didClickLeftButton:) forControlEvents:UIControlEventTouchUpInside];
-        [self.view addSubview:leftButton];
-        _leftButton = leftButton;
-    }
-    return _leftButton;
 }
 
 @end
